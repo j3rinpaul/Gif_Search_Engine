8f5ca284dd49aac3501b049d2e81a03bf6c7cfd0
Jerry <jerrymic2255@gamil.com>
2021-08-02 10:14:47 +0530

    js first project

---
+++ app.js
@@ -0,0 +1,9 @@
+// Function to change the body color to dark
+function myFunction() {
+    document.getElementById("body").style.backgroundColor = "#161616";
+  }
+// return body color to light
+function myDark() {
+document.getElementById("body").style.backgroundColor = "white";
+}
+
