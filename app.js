76b954db4c90e4cdc01dedcf0e06f1a813cfc1e7
Jerry <jerrymic2255@gamil.com>
2021-08-07 11:46:26 +0530

    gif search Engine on going

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-// Function to change the body color to dark
-function myFunction() {
-    document.getElementById("body").style.backgroundColor = "#161616";
-  }
-// return body color to light
-function myDark() {
-document.getElementById("body").style.backgroundColor = "white";
-}
+/*1.Get the text input */ //psuedocosde
+let button = document.querySelector('button');
+button.addEventListener("click",()=>{
+  let text = document.querySelector('input').value;
+  console.log(text);
+})
+let even = document.querySelector('.input_cl')
+even.addEventListener('keyup',function(e){ //for every key or click there is a ton of values which is expressed as e
+  //we are utilising it 
 
+  let text = document.querySelector("input").value;
+  if(e.keyCode === 13){ //key code of enter is 13 by which when we press enter
+    //the condition is satisfied and the function below gets executed
+    pushTo(text);
+  }
+})
+  
+
+function pushTo(text) { //function to push the result to html body
+  var container = document.querySelector(".element");
+  container.innerHTML = text;
+
+}
