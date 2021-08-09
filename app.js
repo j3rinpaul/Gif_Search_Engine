39429d9956f652400cbde74e04858420e288dcf9
Jerry <jerrymic2255@gamil.com>
2021-08-09 20:23:56 +0530

    finalized

--- app.js
+++ app.js
@@ -1,23 +1,57 @@
-/*1.Get the text input */ //psuedocosde
-let button = document.querySelector('button');
-button.addEventListener("click",()=>{
-  let text = document.querySelector('input').value;
-  console.log(text);
-})
+let button = document.querySelector('button'); //grabing the button element
+button.addEventListener("click",()=>{ //adding an event listner click to the button
+  let text = document.querySelector('input').value; //function to be executed after the event
+  api(text);
+});
 let even = document.querySelector('.input_cl')
-even.addEventListener('keyup',function(e){ //for every key or click there is a ton of values which is expressed as e
-  //we are utilising it 
-
+even.addEventListener('keyup',(e)=>{ /*for every key or click there is a ton of informations which is expressed as e
+  we are utilising it */
   let text = document.querySelector("input").value;
-  if(e.keyCode === 13){ //key code of enter is 13 by which when we press enter
-    //the condition is satisfied and the function below gets executed
-    pushTo(text);
+  if(e.keyCode === 13){ /*key code of enter is 13 by which when we press enter
+    the condition is satisfied and the function below gets executed*/
+    api(text);
   }
-})
-  
+});
+// API
+function api(text){
+  if (text == false){
+    var url = "https://api.giphy.com/v1/gifs/search?api_key=Kwt76tthWXcO4cC8UfFjx4lxS8Q9VWQ6&q=github&limit=25&offset=0&rating=g&lang=en";
+    console.log(url);
+    let giphy = new XMLHttpRequest();
+    giphy.open('GET',url);
+    giphy.send();
+    giphy.addEventListener('load',(d)=>{
+      // console.log(d);
+      let load = d.target.response;
+      // console.log(load);
+      pushTo(load);
+    });
+  }
+  else{
+    var url = "https://api.giphy.com/v1/gifs/search?api_key=Kwt76tthWXcO4cC8UfFjx4lxS8Q9VWQ6&q=query&limit=25&offset=0&rating=g&lang=en";
+    url = url.replace('query',text);
+    // console.log(url);
+    let giphy = new XMLHttpRequest();
+    giphy.open('GET',url);
+    giphy.send();
+    giphy.addEventListener('load',(d)=>{ //arrow function
+      // console.log(d);
+      let load = d.target.response;
+      pushTo(load);
+    
+    });
+  }
+}
 
-function pushTo(text) { //function to push the result to html body
-  var container = document.querySelector(".element");
-  container.innerHTML = text;
-
+function pushTo(input) { //function to push the result to html body
+  let response = JSON.parse(input);
+  let imgUrl = response.data;
+  imgUrl.forEach(function(image){
+    var src = image.images.fixed_height.url;//basic object manipulation
+    console.log(src);
+    var foot = document.querySelector('.footer');
+    var container = document.querySelector(".elements");
+    container.innerHTML += "<img src=\"" + src + "\" class=\"element\">";
+    foot.innerHTML = "Refresh for another Search <br> Minor Bug..Fixing it soon... <br><br> Made by Jerry"
+  });
 }
